a755fb42597a518788d116882240a235363f47e8
minitableau <minitableau2002@gmail.com>
2022-11-18 15:24:44 +0100

    pHP

--- pageConnexion/connexion.php
+++ pageConnexion/connexion.php
@@ -2,7 +2,7 @@
 
 if (isset($_POST['login']) && isset($_POST['password'])) {
     if ($_POST['login'] == 'admin' && $_POST['password'] == 'admin') {
-        echo '<b>Bienvenue jeune admin</b>';
+        echo '<b>Bienvenue Admin</b>';
     } else {
         echo '<b>Identifiants incorrects</b>';
     }
