0904b8ca1acac9d0064ae8e0114ca51fd9342646
minitableau <minitableau2002@gmail.com>
2022-11-22 09:19:52 +0100

    php admin 2 pages

--- pageConnexion/connexion.php
+++ pageConnexion/connexion.php
@@ -1,8 +1,11 @@
 <?php
 
+session_start();
+
 if (isset($_POST['login']) && isset($_POST['password'])) {
     if ($_POST['login'] == 'admin' && $_POST['password'] == 'admin') {
-        echo '<b>Bienvenue Admin</b>';
+        $_SESSION['login'] = $_POST['login'];
+        header('Location: admin.php');
     } else {
         echo '<b>Identifiants incorrects</b>';
     }
