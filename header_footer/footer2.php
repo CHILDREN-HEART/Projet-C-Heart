da4bc98df5631053a17a44f0e75fea45e9b7b2fc
Aquaze <88437016+Aquaze@users.noreply.github.com>
2022-12-13 12:27:04 +0100

    la fermeture c'est important

--- header_footer/footer2.php
+++ header_footer/footer2.php
@@ -1,13 +1,13 @@
 <link rel="stylesheet" href="../header_footer/footer2.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
 <div class="flexboxFooter">
-<div class="btn1">
-    <a href="../faq"><input type="button" value="FAQ"/></a>
-</div>
-<div class="btn2">
-    <a href="../forum"><input type="button" value="Forum"/></a>
+    <div class="btn1">
+        <a href="../faq"><input type="button" value="FAQ"/></a>
+    </div>
+    <div class="btn2">
+        <a href="../forum"><input type="button" value="Forum"/></a>
     </div>
     <div class="btn3">
-    <a href="../Quizz"><input type="button" value="Quizz"/></a>
+        <a href="../Quizz"><input type="button" value="Quizz"/></a>
     </div>
 </div>
