0dfa3bdd5acbaeb0efbb458e98e85432d08f69e7
minitableau <minitableau2002@gmail.com>
2022-12-13 14:41:39 +0100

    header 2 (apres co)

--- header_footer/header2.php
+++ header_footer/header2.php
@@ -1,18 +1,20 @@
-<link rel="stylesheet" href="header.css">
+<link rel="stylesheet" href="header2.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
 <div class="flexboxHeader">
     <div>
         <img id="img1" src="../assets/Logo.png" alt="logo">
     </div>
     <div class="flexboxHeader2">
-        <span>Child heart</span>
+        <span>Bienvenue $Parent</span>
     </div>
+
     <div class="flexboxHeader3">
-        <a id="inscription" href="../inscription/index.php">Inscription</a>
+        <a id="inscription" href="/inscription">$Prénom $Nom</a>
     </div>
     <div class="flexboxHeader4">
         <a href="../connexion/index.php" class="logo_connexion"><img id="logo_connexion"
                                                                      src="../assets/logoconnexion.png"
                                                                      alt="logo_connexion"/></a>
     </div>
+
 </div>
--- index.php
+++ index.php
@@ -2,25 +2,11 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8"/>
-    <link
-            href=""
-            rel="stylesheet"
-    />
+    <link href="" rel="stylesheet"/>
     <title>page d'accueil</title>
-    <link
-            rel="stylesheet"
-            href="pagedaccueil.css"
-    />
-    <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1"
-    />
-    <link
-            rel="stylesheet"
-            href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
-    />
-
-
+    <link rel="stylesheet" href="pagedaccueil.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"/>-->
     <link rel="stylesheet" href="header_footer/header.css">
 </head>
 <body>
@@ -31,77 +17,39 @@
 <div style="height: 200px"></div>
 <div class="carroussel">
     <div class="container">
-        <div
-                id="myCarousel"
-                class="carousel slide"
-                data-ride="carousel"
-        >
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
-                <li
-                        data-target="#myCarousel"
-                        data-slide-to="0"
-                        class="active"
-                ></li>
-                <li
-                        data-target="#myCarousel"
-                        data-slide-to="1"
-                ></li>
-                <li
-                        data-target="#myCarousel"
-                        data-slide-to="2"
-                ></li>
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
             </ol>
-
             <div class="carousel-inner">
                 <div class="item active">
-                    <img
-                            class="img3"
-                            src="../assets/main.jpg"
-                            alt="car1"
-                    />
+                    <img class="img3" src="../assets/main.jpg" alt="car1"/>
                     <div class="carousel-caption">
-                        <p class="text1">
-                            C-Heart <br/>
-                            Pour le bien etre de l’enfance
-                        </p>
+                        <p class="text1">C-Heart <br/>Pour le bien etre de l’enfance</p>
                     </div>
                 </div>
-
                 <div class="item">
-                    <img
-                            src="../assets/main.jpg"
-                            alt="car2"
-                    />
+                    <img src="../assets/main.jpg" alt="car2"/>
                     <div class="carousel-caption">
                         <p class="text2">Notre objectif est de vous informer sur l’état de santé de votre enfant
                             lorsqu’il est dans son environnement scolaire</p>
                     </div>
                 </div>
                 <div class="item">
-                    <img
-                            src="../assets/main.jpg"
-                            alt="car3"
-                    />
+                    <img src="../assets/main.jpg" alt="car3"/>
                     <div class="carousel-caption">
                         <p class="text3">Nous vous informons sur la fréquence cardiaque et la température de votre
                             enfant.</p>
                     </div>
                 </div>
             </div>
-
-            <a
-                    class="left carousel-control"
-                    href="#myCarousel"
-                    data-slide="prev"
-            >
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
                 <span class="glyphicon glyphicon-chevron-left"></span>
                 <span class="sr-only">Previous</span>
             </a>
-            <a
-                    class="right carousel-control"
-                    href="#myCarousel"
-                    data-slide="next"
-            >
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
                 <span class="glyphicon glyphicon-chevron-right"></span>
                 <span class="sr-only">Next</span>
             </a>
