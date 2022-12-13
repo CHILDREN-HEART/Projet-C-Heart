7cc90e30802154472bca2103d4a9bfaaff908831
Aquaze <88437016+Aquaze@users.noreply.github.com>
2022-12-13 10:46:00 +0100

    footer v0

--- header_footer/footer1.php
+++ header_footer/footer1.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="../header_footer/footer.css">
 <div class="flexboxFooter">
     <div class="flexboxFooter1">
+        
     </div>
 </div>
---
+++ header_footer/footer2.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="../header_footer/footer2.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+<div class="flexboxFooter">
+<div class="btn1">
+    <a href="../index.php"><input type="button" value="Retour"/></a>
+    </div>
+</div>
