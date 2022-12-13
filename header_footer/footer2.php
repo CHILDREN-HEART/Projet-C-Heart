440b0e73283c2bbec96fc67e76221951cfa57745
Aquaze <88437016+Aquaze@users.noreply.github.com>
2022-12-13 12:20:46 +0100

    footer v1

--- header_footer/footer2.php
+++ header_footer/footer2.php
@@ -2,6 +2,12 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
 <div class="flexboxFooter">
 <div class="btn1">
-    <a href="../index.php"><input type="button" value="Retour"/></a>
+    <a href="../faq"><input type="button" value="FAQ"/></a>
+</div>
+<div class="btn2">
+    <a href="../forum"><input type="button" value="Forum"/></a>
+    </div>
+    <div class="btn3">
+    <a href="../Quizz"><input type="button" value="Quizz"/></a>
     </div>
 </div>
