165310b68d094ce0fd566825439cb300b0693a29
Aquaze <88437016+Aquaze@users.noreply.github.com>
2023-01-03 10:55:10 +0100

    supp

--- pagecapteurprof/index.php
+++ pagecapteurprof/index.php
@@ -39,25 +39,14 @@
             <span class="moyenchiffre">°C</span>
         </div>
     </div>
-    <div style="height: 2px; background: black; width: 95%; margin-left: auto; margin-right: auto;"></div>
-    <div style="height: 50px;"></div>
-    <div class="conteneur">
-        <div class="conteneurchild">
-            <span class="titre3">Fréquence Cardiaque</span>
-            <canvas id="canvas">
-        </div>
-
-        <div class="divider"></div>
-        <div class="conteneurchild">
-            <span class="titre3">Température</span>
-            <canvas id="canvas2">
-        </div>
-    </div>
+    
+    
     <div style="height: 50px;"></div>
     <div style="height: 2px; background: black; width: 95%; margin-left: auto; margin-right: auto;"></div>
     <div style="height: 150px;"></div>
 
 </main>
+
 
 
 <footer>
