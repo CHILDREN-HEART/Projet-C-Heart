bc08128fa47728c8efe3664a35aa24c55db2ab44
mathys <mr.red111111@gmail.com>
2023-01-14 00:39:16 +0100

    WORLD RECORDDDD

--- quizz/index.php
+++ quizz/index.php
@@ -1,29 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Quizz</title>
-    <link rel="stylesheet" href="quizz.css"/>
+    <link rel="stylesheet" href="quizz.css" />
 </head>
+
 <body>
-<header>
-    <?php require "../header_footer/header1.php"; ?>
-</header>
+    <header>
+        <?php require "../header_footer/header1.php"; ?>
+    </header>
 
-<div class="quiz-container" id="quiz">
-    <div class="quiz-header">
-        <h2 id="question">Question text</h2>
-        <ul id="answers">
-        </ul>
+    <div class="quiz-container" id="quiz">
+        <div class="quiz-header">
+            <h2 id="question">Question text</h2>
+            <ul id="answers">
+            </ul>
+        </div>
     </div>
-</div>
-<input type="submit" id="submit" value="validé" class="validé">
-<script src="quizz.js"></script>
-<footer>
-    <?php require "../components/footer1.php"; ?>
-</footer>
+    <div class="buttons-container" style="display: flex; justify-content: center;">
+        <input type="submit" id="submit" value="valider" class="btn">
+    </div>
+    <script src="quizz.js"></script>
+    <footer>
+        <?php require "../components/footer1.php"; ?>
+    </footer>
 </body>
-</html>
 
+</html>
