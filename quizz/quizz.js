6042ef602271924826f301b1bdd23e6caea7e715
mathys <mr.red111111@gmail.com>
2023-01-17 20:37:58 +0100

    Super le manuel les gars 👍

--- quizz/quizz.js
+++ quizz/quizz.js
@@ -88,8 +88,7 @@ function loadQuiz() {
             loadQuiz();
         } else {
             quiz.innerHTML = `<h2>Vous avez répondu correctement à ${score}/${quizData.length} questions.</h2>`;
-            buttonsContainer.innerHTML = `<a class="btn" href="quizz/index.php">Quizz</a><br/><button onclick="location.reload()" class="btn">Recommencer</button>`;
-            buttonsContainer.innerHTML= '<a class="btn" href="quizz/reponse.php">Quizz</a><br/><button onclick="location.reload()" class="btn">Réponse</button>'
+            buttonsContainer.innerHTML = '<a class="btn" href="/quizz/reponse.php">Réponses</a><br/><a class="btn" href="/quizz/index.php">Recommencer</a>';
         }
     });
 }
