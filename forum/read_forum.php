21a5a75d5a264254a8cbf73f1ddd11aaac55d2c7
minitableau <minitableau2002@gmail.com>
2023-01-17 14:23:42 +0100

    forum fini

---
+++ forum/read_forum.php
@@ -0,0 +1,26 @@
+<?php
+//$host = "herogu.garageisep.com";
+//$dbname = "tw7TQUoQ7u_cheart";
+//$username = "HCjpLtsbkh_cheart";
+//$password = "dRQscVBnTH6HWDYK";
+$host = "localhost";
+$dbname = "c_heart";
+$username = "root";
+$password = "";
+
+$dsn = "mysql:host=$host;dbname=$dbname";
+// récupérer tous les utilisateurs
+$sql = "SELECT * FROM forum ORDER BY numero_article DESC";
+
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    $stmt = $pdo->query($sql);
+
+    if ($stmt === false) {
+        die("Erreur");
+    }
+
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+?>
---
+++ forum/read_forum_message.php
@@ -0,0 +1,28 @@
+<?php
+//$host = "herogu.garageisep.com";
+//$dbname = "tw7TQUoQ7u_cheart";
+//$username = "HCjpLtsbkh_cheart";
+//$password = "dRQscVBnTH6HWDYK";
+$host = "localhost";
+$dbname = "c_heart";
+$username = "root";
+$password = "";
+
+$numero_article = filter_input(INPUT_GET, 'numero_article', FILTER_SANITIZE_NUMBER_INT);
+
+$dsn = "mysql:host=$host;dbname=$dbname";
+$sql = "SELECT * FROM forum_message WHERE numero_article = :numero_article";
+
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':numero_article', $numero_article);
+    $stmt->execute();
+    if ($stmt === false) {
+        die("Erreur");
+    }
+
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+?>
