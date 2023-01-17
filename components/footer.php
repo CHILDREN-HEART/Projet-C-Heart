5bdb88eb9c8910ff5db14302278924a78c1586c7
Aquaze <88437016+Aquaze@users.noreply.github.com>
2023-01-17 17:23:36 +0100

    trait blanc footer accueil

--- components/footer.php
+++ components/footer.php
@@ -4,10 +4,13 @@
     <div class="btn">
         <a href="./faq"><input type="button" value="FAQ"/></a>
     </div>
+    <div style="background: white; height: 40px; width: 2px;"></div>
     <div class="btn">
         <a href="./forum"><input type="button" value="Forum"/></a>
     </div>
+    <div style="background: white; height: 40px; width: 2px;"></div>
     <div class="btn">
         <a href="./quizz"><input type="button" value="Quizz"/></a>
     </div>
+    
 </footer>
