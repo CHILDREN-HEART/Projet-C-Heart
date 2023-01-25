beb8c9f199ff4f6aa6697255972a5dc73681f86e
minitableau <minitableau2002@gmail.com>
2023-01-25 18:36:59 +0100

    countournement pb header + quiz tentative de reparation

--- components/footer.php
+++ components/footer.php
@@ -20,6 +20,6 @@
         </div>
         <div id="barre"></div>
         <div class="btn-footer">
-            <a href="./quiz/quiz.php"><input type="button" value="Quiz" /></a>
+            <a href="./quiz/index.php"><input type="button" value="Quiz" /></a>
         </div>
     </div>
