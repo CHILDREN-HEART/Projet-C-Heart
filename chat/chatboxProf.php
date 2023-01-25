22702523a5c03effdd8cf9097d5a9fb184b2d1cb
Teroaz <neoxdelta@gmail.com>
2023-01-25 20:55:22 +0100

    chat ?

--- chat/chatboxProf.php
+++ chat/chatboxProf.php
@@ -28,6 +28,7 @@ echo "<div>
     <input type='hidden' id='prof_id' value='" . $profID . "'>
     <input type='hidden' id='sent_by_student' value='0'>
     <button id='send'>Envoyer</button>
+</div>
 </div>";
 
 echo "<script src='./script.js'>";
